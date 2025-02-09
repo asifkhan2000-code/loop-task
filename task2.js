169157e2dc821ec75ed3fa3a27220004351f6eb3
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-10 01:49:40 +0600

    task2 complete

---
+++ task2.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+let num = 61;
+while (num <= 100) {
+    if (num % 2 != 0) {
+        console.log(num);
+    }
+    num++;
+}
+let j = 78;
+while (j <=98) {
+    if(j % 2 == 0) {
+        console.log(j);
+    }
+    j++;
+}
