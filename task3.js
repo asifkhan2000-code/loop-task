293dce4caa7e092f7692b375afdb85b4eca2dd0d
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-10 01:56:47 +0600

    task3 completed

---
+++ task3.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+let i = 81;
+let sum = 0;
+while (i <= 131) {
+    sum = sum + i;
+    i += 2;
+}
+console.log("sum of all the odd numbers from 81 to 131", sum);
+
+let j = 206;
+let Sum = 0;
+while (j <= 311) {
+    Sum = Sum + j;
+    j +=2;
+}
+console.log("sum of all the even numbers from 206 to 311", Sum);
