9a0470a032e901f9efe42118c130ba49b6d22349
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-10 01:41:41 +0600

    loopTask1 done

---
+++ task1.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+let num = 1;
+while (num <= 60) {
+    console.log(
+        `${num}. I will invest at least 6 hrs every single day for next 60 days.`
+    );
+    num++;
+}
