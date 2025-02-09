8471f6f291fc92dcbb3232bb84a6b51b00e42c40
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-10 02:06:11 +0600

    task4 done

---
+++ task4.js
@@ -0,0 +1,10 @@
+function multiplicationTable (number) {
+    let i = 1;
+    while(i <= 10) {
+        console.log(`${i} X ${number} = ${i * number}`);
+        i++;
+    }
+}
+multiplicationTable(5);
+multiplicationTable(6);
+multiplicationTable(9);
