eb19c336b67e989bdf1e34bd5b340e5428e57c00
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-14 19:00:30 +0600

    done

--- task8.js
+++ task8.js
@@ -1,6 +1,16 @@
+// 1st method
 for (let num = 2; num <= 100; num++) {
     if (Math.sqrt(num) % 1 === 0) {
         console.log("First square number encountered:", num);
         break;
     }
+}
+// second method
+let i = 2;
+while (true) {
+    if (Number.isInteger(Math.sqrt(i))) {
+        console.log("First square number encountered, it is", i);
+        break;
+    }
+    i++;
 }
